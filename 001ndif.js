6949f0c15d9075ca4ec8f1974ca54250bae07442
Diana <oganauskiene@gmail.com>
2022-05-06 10:05:19 +0300

    nd

--- 001ndif.js
+++ 001ndif.js
@@ -1,3 +1,14 @@
+console.log("--------------");
+let valandos = 22;
+
+if (valandos >= 6 && valandos < 12) {
+  console.log("Labas rytas", valandos);
+} else if (valandos >= 12 && valandos < 18) {
+  console.log("Laba diena", valandos);
+} else if (valandos >= 18 && valandos < 22) {
+  console.log("Labas vakaras", valandos);
+} else console.log("Labanaktis", valandos);
+
 //1. Naudokite funkcija rand(). Sukurkite du kintamuosius ir naudodamiesi funkcija rand() jiems priskirkite atsitiktines reikšmes nuo 0 iki 4. Didesnę reikšmę padalinkite iš mažesnės. Atspausdinkite rezultatą naudodami console.log()
 console.log("1------------");
 
@@ -123,13 +134,52 @@ console.log("skai3 =", skai3);
 console.log("skai4 =", skai4);
 
 //5. Naudokite funkcija rand(). Atspausdinkite 3 skaičius naudodami console.log() nuo -10 iki 10. Prie skaičių mažesnių už 0 iš abiejų pusių atspauzdinkite “+” simbolius,  didesnių už 0 iš abiejų pusių atspauzdinkite “-” simbolius, o prie 0 “*” simbolius
-// console.log("5------------");
 
 console.log("5------------");
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 3; i++) {
+  let value = random(-10, 10);
+
+  if (value < 0) {
+    console.log(`+${value}+`);
+  }
+  if (value > 0) {
+    console.log(`-${value}-`);
+  }
+  if (value === 0) {
+    console.log(`*${value}*`);
+  }
+}
+// 6. Įmonė parduoda žvakes po 1 EUR. Perkant daugiau kaip už 1000 EUR taikoma 3 % nuolaida, daugiau kaip už 2000 EUR - 4 % nuolaida. Parašykite skriptą, kuri skaičiuos žvakių kainą ir atspausdintų atsakymą su console.log() kiek žvakių ir kokia kaina perkama. Žvakių kiekį generuokite ​rand()​ funkcija nuo 5 iki 3000.
+console.log("6------------");
+
+let zvakiuKiekis = random(5, 3000);
+console.log(zvakiuKiekis);
+let zvakesKaina = 1;
+console.log(zvakesKaina);
+
+if (zvakiuKiekis >= 1000 && zvakiuKiekis < 2000) {
+  zvakesKaina = zvakesKaina * zvakiuKiekis * 0.3;
+  console.log(`Perkant ${zvakiuKiekis} žvakių, taikoma 3 % nuolaida`);
+} else if (zvakiuKiekis >= 2000) {
+  zvakesKaina = zvakesKaina * zvakiuKiekis * 0.4;
+  console.log(`Perkant ${zvakiuKiekis} žvakių, taikoma 4 % nuolaida`);
+}
+
+//7. Naudokite funkcija rand(). Sukurkite tris kintamuosius su atsitiktinėm reikšmėm nuo 0 iki 100. Paskaičiuokite jų aritmetinį vidurkį. Ir aritmetinį vidurkį atmetus tas reikšmes, kurios yra mažesnės nei 10 arba didesnės nei 90. Abu vidurkius atspausdinkite su console.log()
+console.log("7------------");
 function random() {
-  const random = Math.floor(Math.random() * 10) - 9;
+  const random = Math.floor(Math.random() * 101);
   return random;
 }
-if () {
+const kintamieji = random[(k, l, m)];
+console.log(kintamieji);
 
-}
+// const KintamujuSuma = 0;
+// console.log(k1, k2, k3);
+// if (k1 + k2 + k3 / length) {
+//   aritmetinisVidurkis = console.log();
+// }
