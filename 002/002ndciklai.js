a73dd36d898908c3d569d58aadbd335f9724f3f1
Diana <oganauskiene@gmail.com>
2022-05-10 14:30:32 +0300

    Paskaita masyvai

--- 002/002ndciklai.js
+++ 002/002ndciklai.js
@@ -153,25 +153,124 @@ while (skaiciuSuma1 < 100 || skaiciuSuma2 < 100) {
   console.log(`${skaiciuSuma2} skaiciu yra antrame stulpelyje`);
 }
 
-
 // 15 console.log’e, naudojant ciklą atspausdinti po porą skirtingų atsitiktinių skaičių nuo 0 iki 10 (abu skaičiai vienoje eilutėje). Ciklą kartoti kol bus sugeneruota po tris arba daugiau nelyginių skaičių  (skaičiai atskiruose stulpeliuose)
-console.log("15------------------------");
-let skaiciuok1 = 0;
-let skaiciuok2 = 0;
+// console.log("15------------------------");
+// let skaiciuok1 = 0;
+// let skaiciuok2 = 0;
 
-do {
-  let randomSk1 = rand(0, 10);
-  let randomSk2 = rand(0, 10);
-  console.log(`${randomSk1}, ${randomSk2}`);
+// do {
+//   let randomSk1 = rand(0, 10);
+//   let randomSk2 = rand(0, 10);
+//   console.log(`${randomSk1}, ${randomSk2}`);
 
-  if (randomSk1 % 2 != 0) {
-    skaiciuok1++;
-  }
-  if (randomSk2 % 2 != 0) {
-    skaiciuok2++;
-  }
-if (skaiciuok1 >= 3 && skaiciuok2 >= 3) {
-  break;
+//   if (randomSk1 % 2 != 0) {
+//     skaiciuok1++;
+//   }
+//   if (randomSk2 % 2 != 0) {
+//     skaiciuok2++;
+//   }
+// if (skaiciuok1 >= 3 && skaiciuok2 >= 3) {
+//   break;
+// }
+// }
+// while (skaiciuok1 != 3 || skaiciuok2 != 3);
+
+// 1. console.log’e, naudojant ciklą atspausdinti penkias eilutes ‘Labas’;
+for (let i = 0; i < 5; i++) {
+  console.log('Labas');
+}
+// 2.console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje);
+for (let i=0; i <= 4; i++) {
+  console.log(i);
+}
+// 3. console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje);
+let a = 0;
+
+for (let i = 0; i <= 40; i+= 10) {
+console.log(i);
+while (a) {
+ if(++a > 100) {
+  console.log('avarija');
+  break; //saugiklis
 }
 }
-while (skaiciuok1 != 3 || skaiciuok2 != 3);
+}
+// 4. console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje);
+for (let i = 49; i <= 53; i++) {
+  console.log(i);
+}
+// 5. console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 4 3 2 1 0 (vienas skaičius vienoje eilutėje);
+for (let i = 4; i >= 0; i--) {
+  console.log(i);
+}
+// 6. console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 2 4 6 8 (vienas skaičius vienoje eilutėje, daugybos konsolėje nenaudoti!, if’ų nenaudoti, ciklas for);
+console.log('------------');
+for (let i =0; i <= 8; i+=2) {
+  console.log(i);
+}
+// 7. console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje);
+console.log("7------------------------");
+
+for (let i = 0; i < 5; i++) {
+  num1 = rand(0, 10)
+  console.log(num1);
+}
+// 8 console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5;
+console.log("8------------------------");
+let num2 = 0;
+
+while (num2 != 5) {
+  num2 = rand(0, 10)
+  console.log(num2);
+}
+// 9 console.log’e, naudojant ciklą atspausdinti atsitiktinius skirtingus skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Ciklas turi pasibaigti tada, kai atsitiktinių skaičių suma viršija 100;
+console.log("9------------------------");
+
+let sum1 = 0;
+
+while (sum1 <= 100) {
+  sum1 += rand(1, 10)
+  console.log(sum1); 
+}
+// 10 console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5 arba 7; Suskaičiuoti kiek ciklų prasisuko;
+console.log("10 ------------------------");
+
+let ciklai = 0;
+let num3 = 0;
+
+while (num3 != 5 && num3 != 7) {
+  num3 = rand(0, 10)
+  console.log(num3);
+  ciklai++
+}
+console.log(`Prasisuko ${ciklai} ciklai`);
+
+//11 console.log’e, naudojant ciklą atspausdinti atsitiktinius skirtingus skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Ciklas turi pasibaigti tada, kai atsitiktinių skaičių suma viršija 20, bet ne anksčiau nei po 11 ciklų;
+console.log("11------------------------");
+let ciklai1 = 0;
+let sum2 = 0;
+let num4 = 0;
+
+while (sum2 < 20 && ciklai1 < 11) {
+  sum2 = rand(0, 10)
+  ciklai1++
+  console.log(rand(0, 10));
+}
+// 12 console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Ciklą kartoti kol bus sugeneruoti trys nelyginiai skaičiai;
+console.log("12------------------------");
+
+let nelyginiaiS = 0;
+
+While (nelyginiaiS) {
+
+}
+console.log('egzas');
+let pelyte = 0;
+let kateSukasi = 0;
+
+while (pelyte <= 20) {
+  pelyte += rand(3, 5)
+  kateSukasi++;
+}
+console.log(`Kate pagavo ${pelyte} ziurkes`);
+console.log(`apisuko ${kateSukasi} kartus aplink nama`);
---
+++ 004/app.js
@@ -0,0 +1,64 @@
+'úse strict'; //gaudo klaidas https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+//masyvas //objektas
+const n = ['Petras', 'Asilas', 'Barsukas', 'Kate']
+
+console.table(n)
+console.log(n[1]);
+
+n[1] = 'Kazys' //+naujas gyventojas
+
+// n[4] = 'Suo'  //naujas gyventojas 4 aukste
+n.pop() // isvaryt is namo, ismeta kas paskutiniam aukste masyvo pabaigoj
+n.shift() // ismesti is rusio, rasyti i skliaustelius nieko nereikia
+
+n.push('Suo'); //jei namas 5 aukstu, bet mes norim apgyvendinti suni kitame sekanciame aukste
+n.unshift('Pele'); // ideti is apacios ( apgyvendinti rusyje). Unshift visada = 0
+
+n.splice(2, 1) // universalus, neskirtas trinti is masybo, bet naudojamas ir masyve. Ismesti is vidaus. Funkcija pjaustanti masyva (2-barsuko gyvenamas aukstas, 1 - vnt auksto iskirpti i virsu)
+
+
+
+console.table(n);
+console.log(n.length); //aukstu skaicius
+
+//cilkas
+for (let i = 0; i < n.length; i++) {// i < visaga su n.lenght. Praeiname per visa masyva, jei nezinom kiek aukstu naudojam while cikla
+
+  console.log(n[i]); // i pastoviai keisis
+}
+//masyvas masyve
+
+const nPlus = [
+  [4, 7, 12],
+  [85, 17, 12],
+  [10, 33, 74],
+  [26, 41, 99],
+]
+console.table(nPlus);
+console.table(nPlus[1][0]); //85 atspausdins
+console.table(nPlus[3][2]); //99 atspausdins
+
+for (let i = 0; i < nPlus.length; i++) { //ciklina per aukstus
+  for (let k = 0; k < nPlus[i].length; k++) { //ciklas cikle //ciklina per butus
+    // console.log(nPlus[i]);
+    // console.log(nPlus[i][k]);
+  }
+}
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//sukurti masyva is 100 elementu, kuriu  random skaiciu
+const naujasNAmas = [];
+
+
+for (let i = 0; i < 10; i++) {
+  const aukstas = [];
+  for (let i = 0; i < 10; i++) { // jei i < 10 atsitiktinis, vietoj konkrecios reiksmes 10 rasom rand(3, 10)
+    aukstas.push(rand(10, 99));
+  }
+  naujasNAmas.push(aukstas);
+}
+console.table(naujasNAmas);
