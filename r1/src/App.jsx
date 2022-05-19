a0f6e177be4ebcc86694d28b80c01fdc83165962
Diana <oganauskiene@gmail.com>
2022-05-19 14:13:48 +0300

    randcolor

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 
+const katinukai = ['Pilkis', 'Murkis', 'Rainis'];
 
 function App() {
   const [spalva, setSpalva] = useState('yellow');
   const [skaicius, setSkaicius] = useState(1);
+  const [kv, setKv] = useState([]);
   //state reiksme, kitas funkcija
 
   // const mygtukas = () => {
@@ -35,6 +37,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 style={{ color: spalva }}>State {skaicius}</h1>
+        {
+          katinukai.map((k, i) => <div key={i}>{k}</div>)
+        }
+
         <button onClick={beArgumentu}>Be!</button>
         <button onClick={() => suArgumentu('Labas')}>Su!</button>
         <button onClick={keistiSpalva}>Kita spalva</button>
@@ -52,7 +58,8 @@ export default App;
 // onClick virsuje du galimi budai su arba be argumento
 
 //state - komponento viduje.
-//hug - reactas paraso funkcija useeState, o ji padeda susikurti state komponento viduje
+//hooks - reactas paraso funkcija useeState, o ji padeda susikurti state komponento viduje
 //turim const [a, b] ir priskiriame masyva [1,2] a guas 1, b gaus 2.
 //  const a=[1,2][0] const b=[1,2][1]
 //state negalima keisti, i ji pushint. Daryti kopija i ja sukelti ir tada ja perkelti
+//list -
