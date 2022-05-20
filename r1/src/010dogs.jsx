153b1f75fa5536d47b4a43695ef482c4f1fcc5d9
Diana <oganauskiene@gmail.com>
2022-05-20 13:43:35 +0300

    dogs

---
+++ r1/src/010dogs.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './App.css';
+import randColor from './Functions/randColor';
+
+const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
+dogs.sort();
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="kvc">
+          {
+            dogs.map((d, i) => <div className="kv" key={i}>{i}. {d}</div>)
+          }
+        </div>
+        <div className="kvc">
+          {
+            dogs.map((d, i) => <div className="circle" key={i}>{i}. {d}</div>)
+          }
+        </div>
+        <div className='kvc'>
+          {
+            dogs.map((d, i) => <div className={i % 2 === 0 ? 'kv' : 'circle'} key={i}>{i}. {d}</div>)
+          }
+        </div>
+        <div className='kvc'>
+          {
+            dogs.map((d, i) => <div className={i % 2 === 0 ? 'kv' : 'circle'} key={i}>{i}. {d}</div>)
+          }
+        </div>
+        <div className='kvc'>
+          {
+            dogs.map((d, i) => <div className={dogs.lenght > 6 ? 'green' : 'red'} key={i}>{i}. {d}</div>)
+          }
+          {/* txt.charAt(0).toUpperCase() + txt.substr(1); */}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// 1. Atvaizduoti masyvą dogs. Kiekvienas šuo atskirame kvadrate.
+
+// 2. Atvaizduoti masyvą dogs. Kiekvienas šuo atskirame apskritime. Šunys turi būti išrūšiuoti nuo ilgiausio žodžio iki trumpiausio, o apskritimai sunumeruoti nuo 1 iki galo.
+
+// 3. Atvaizduoti masyvą dogs. Poriniuose indeksuose esančius šunis atvaizduoti kvadratuose, neporinius apskritime.
+
+// 4. Atvaizduoti masyvą dogs. Šunis, kurie prasideda didžiąja raide praleisti (neatvaizduoti).
+
+// 5. Naudojant masyvą dogs atvaizduoti skaičius, kurie yra lygūs žodžių masyve ilgiui. Skaičius, didesnius nei 6 atvaizduoti žaliai, kitus raudonai.
