a12f72c68ac2e9944e9eebeb2f0a709fa969f103
Diana <oganauskiene@gmail.com>
2022-05-26 17:17:28 +0300

    new app

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,34 +1,12 @@
-import { useEffect, useState } from 'react';
 import './App.scss';
 
 
 
-
 function App() {
-  // useEffect(() => {
-  //   console.log(document.querySelector('#target'));
-
-  // })
-
-  const [timer, setTimer] = useState(0);
-
-  useEffect(() => {
-    const flagInterval = setIntreval(() => {
-      console.log('fired');
-      setTimer(timer + 1);
-    }, 1000);
-    return () => clearInterval(flagInterval); //sustabdys buvusiam renderenge
-  });
-
-
-  // setInterval(() => setTimer(timer + 1), 1000);
-
-
-
 
   return (
     <div>
-      <h1 id='target'>Labas {timer}</h1>
+
 
     </div>
   )
