8c92695479e914a49acf1d300df886c88cc06c59
Diana <oganauskiene@gmail.com>
2022-06-08 14:44:21 +0300

    bla

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,11 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import './App.scss';
 import listReducer from './Reducers/listReducer';
 
 function App() {
 
   const [list, listDispach] = useReducer(listReducer, []);
+  const [numInput, setNumInput] = useState();
 
   const newList = () => {
     const action = {
@@ -47,13 +48,29 @@ function App() {
     }
     listDispach(action);
   }
-  const addBlack = () => {
+
+  const add = () => {
     const action = {
       type: 'add'
     }
     listDispach(action);
   }
+  const bla = n => {
+    const action = {
+      type: 'bla',
+      payload: n
+    }
+    listDispach(action);
+  }
+  const goNum = () => {
+    setNumInput(numInput);
+    const action = {
+      type: 'go_num',
+      payload: numInput
+    }
+    listDispach(action);
 
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -65,12 +82,13 @@ function App() {
           <button onClick={f5000}>Filter more 5000</button>
           <button onClick={f4000}>Filter less 4000</button>
           <button onClick={freset}>Filter Reset</button>
-          <button onClick={addBlack}>Add to list</button>
-
+          <button onClick={add}>Add to List</button>
+          <input type="text" value={numInput} onChange={e => setNumInput(e.target.value)}></input>
+          <button onClick={goNum}>Input</button>
         </div>
         <div className="kvc">
           {
-            list.map((o, i) => o.show ? <div key={i} className="kv" style={{ backgroundColor: o.color }}><i>{o.number}</i></div> : null)
+            list.map((o, i) => o.show ? <div key={i} className="kv" onClick={() => bla(o.number)} style={{ backgroundColor: o.color }}><i>{o.number}</i></div> : null)
           }
         </div>
 
--- r1/src/Reducers/listReducer.js
+++ r1/src/Reducers/listReducer.js
@@ -25,22 +25,26 @@ function listReducer(state, action) {
         return 0;
       });
       break;
+    case "DefSortList":
+      newState = [...state].sort((a, b) => a.row - b.row);
+      break;
     case "f5000":
       newState = state.map(o => o.number > 5000 ? { ...o, show: true } : { ...o, show: false })
       break;
     case "f4000":
       newState = state.map(o => o.number < 4000 ? { ...o, show: true } : { ...o, show: false })
       break;
+    case "freset":
+      newState = state.map(o => ({ ...o, show: true }))
+      break;
     case 'add':
       newState = [...state, {
         number: ("" + rand(0, 9999)).padStart(4, 0),
-        color: "black",
+        color: '#000000',
         show: true,
-        row: state.lenght
+        row: state.length
       }];
-
       break;
-
     default:
       newState = [...state];
   }
