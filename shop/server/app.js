ee0efba36d26cb9b1745070b3f2f9ed3ea24e46f
Diana <oganauskiene@gmail.com>
2022-06-30 14:06:29 +0300

    update

--- shop/server/app.js
+++ shop/server/app.js
@@ -56,6 +56,19 @@ WHERE id = ?
   });
 });
 
+//edit
+app.put("/admin/cats/:id", (req, res) => {
+  const sql = `
+  UPDATE cats
+  SET title = ? 
+  WHERE id = ?
+  `;
+  con.query(sql, [req.body.title, req.params.id], (err, result) => {
+    if (err) throw err;
+    res.send({ result, msg: { text: 'OK, Cat updated', type: 'success' } });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Bebras klauso porto Nr ${port}`);
 });
